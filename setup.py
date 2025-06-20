41b6a0107842fae2064094953ef03a76467665bd
Revanth Polthi <polthirevanthnaidu@gmail.com>
2025-06-19 19:55:30 -0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI project',
+    version = '0.0.0',
+    author= ' Revanth Polthi',
+    auhtor_email = 'polthirevanth@gmail.com',
+    packages= find_packages(),
+    install_reuires = []
+)
